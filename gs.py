3fe57b7e2d8bfb3cc6d7d9986fc7f9321c128c2f
ErikTeixeira <erik.barbosa515@gmail.com>
2023-06-04 08:42:04 -0300

    fazendo se o usuario colocar "N"

--- gs.py
+++ gs.py
@@ -6,12 +6,22 @@ def cadastro():
 
 
 #Se o usuário colocar "S", ele precisa colocar o email e a senha
-def cadastro_sim(cadastro):
-    if cadastro == "S":
-        email = input("Digite seu email: ")
-        senha = input("Digite sua senha: ")
+def cadastro_sim():
+    email = input("Digite seu email: ")
+    senha = input("Digite sua senha: ")
 
     return email,senha
 
+#Se o usuário colocar "N", ele precisa fazer um cadastro com emal, senha, cep
+    email = input("Digite seu email: ")
+    senha = input("Digite a senha que você vai utilizar: ")
+    cep =input("Digite o seu CEP")
 
-cadastro_sim(cadastro())
+    return email,senha,cep
+
+
+resposta = cadastro()
+if resposta == "S":
+    cadastro_sim()
+elif resposta == "N":
+    cadastro_nao()
