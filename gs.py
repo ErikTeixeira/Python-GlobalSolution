0c4cddcaec90135a53fecdc9768f03db9c6a006a
ErikTeixeira <erik.barbosa515@gmail.com>
2023-06-04 21:15:52 -0300

    arrumei o código

--- gs.py
+++ gs.py
@@ -19,10 +19,9 @@ def cadastro_sim():
 def cadastro_nao():
     email = input("Digite seu email: ")
     senha = input("Digite a senha que você vai utilizar: ")
-    cpf = input("Digite o seu CPF")
     cep =input("Digite o seu CEP: ")
 
-    return [email,senha,cpf,cep]
+    return [email,senha,cep]
 
 #O usuário escolhe se ele vai ser um doador ou um donatário 
 def escolher_tipo_usuario():
@@ -33,11 +32,11 @@ def escolher_tipo_usuario():
 
 
     if tipo_usuario == 1:
-        usuario = print("Você escolheu ser um doador, obrigado por nós ajudar!")
+        print("Você escolheu ser um doador, obrigado por nós ajudar!")
     elif tipo_usuario == 2:
-        usuario = print("Você escolheu ser um donatário, responda algumas perguntas para sabermos se você é apto a receber doações")
+        print("Você escolheu ser um donatário, ajudaremos você em breve!")
 
-    return usuario
+    return tipo_usuario
 
 #Sé o usuário escolher ser doador ele vai escolher se quer escolher dinheiro ou alimento
 def doador():
@@ -47,34 +46,17 @@ def doador():
 
     return escolha
 
-#Usuário escolher doar alimentos
+#Usuário doar alimentos
 def doacao_alimento():
     doar = input("Você escolheu doar alimentos, Você pode doar alimentos não perecíveis como: \nArroz, Macarrão, Feijão, Farinha de trigo, Açúcar, Sal.\nEntre outros alimentos, no seu email uma mensagem será enviada para você saber como enviar. Muito obrigado por estar querendo ser um doador.")
 
     return doar
 
-#Usuário escolher doar dinheiro
+#Usuário doar dinheiro
 def doacao_dinheiro():
     doar = float(input("Você escolheu doar dinheiro, quanto você vai querer doar? "))
 
     return doar
-
-#Sé o usuário escolher ser donatário ele vai ter que verificar as suas necessidades e renda
-def donatario():
-    emprego = input("Você está trabalhando? (S/N) ")
-    renda = float(input("Digite sua renda mensal: "))
-    moradia = input("Você possui moradia? (S/N) ")
-
-    return emprego,renda,moradia
-
-#Função para ferificar se o usuário é empregado
-def situacao_emprego(emprego):
-    if emprego == "S":
-        empregado = input("autonomo")
-    elif emprego == "N":
-        desempregado = input("")
-    
-    return empregado, desempregado
 
 
 #Sistema principal
@@ -84,13 +66,14 @@ if tem_cadastro == "S":
 elif tem_cadastro == "N":
     dados_cadastro = cadastro_nao()
 
-escolher_tipo_usuario()
+tipo_usuario = escolher_tipo_usuario()
 
-escolha_doacao = doador()
-if escolha_doacao == 1:
-    dados_doar = doacao_alimento()
-elif escolha_doacao == 2:
-    dados_doar = doacao_dinheiro()
+if tipo_usuario == 1:
+    escolha = doador()
+    if escolha == 1:
+        doacao_alimento()
+    elif escolha == 2:
+        doacao_dinheiro()
+# elif tipo_usuario == 2:
+#     donatário()
 
-donatario()
-situacao_emprego()
